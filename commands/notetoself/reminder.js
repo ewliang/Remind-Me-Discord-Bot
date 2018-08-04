2ea133e8e2eb5767610cf2019d8f86107c788ea8
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-08-03 21:48:52 -0700

    Added code for reminder command.

--- commands/notetoself/reminder.js
+++ commands/notetoself/reminder.js
@@ -1,4 +1,5 @@
 const commando = require('discord.js-commando');
+const moment = require('moment');
 
 class ReminderCommand extends commando.Command {
   constructor(client) {
@@ -12,7 +13,36 @@ class ReminderCommand extends commando.Command {
 
   async run(message, args) {
     let splitMessage = args.split(' ');
-    message.reply('YOU JUST SAID: ' + args);
+    var filteredMessage = args.replace(splitMessage[0], '');
+    function reminder() {
+      message.reply("\n**REMINDER:**\n" + filteredMessage);
+    }
+    switch(splitMessage[0].slice(-1)) {
+      case 's': {
+        var msDelay = splitMessage[0].slice(0, -1) * 1000;
+        message.reply("Your reminder has been set. I will remind you in " + splitMessage[0].slice(0, -1) + "seconds.");
+        setTimeout(reminder, msDelay);
+        break;
+      }
+      case 'm': {
+        var msDelay = splitMessage[0].slice(0, -1) * 60000;
+        message.reply("Your reminder has been set. I will remind you in " + splitMessage[0].slice(0, -1) + "minutes.");
+        setTimeout(reminder, msDelay);
+        break;
+      }
+      case 'h': {
+        var msDelay = splitMessage[0].slice(0, -1) * 3600000;
+        message.reply("Your reminder has been set. I will remind you in " + splitMessage[0].slice(0, -1) + "hours.");
+        setTimeout(reminder, msDelay);
+        break;
+      }
+      case 'd': {
+        var msDelay = splitMessage[0].slice(0, -1) * 86400000;
+        message.reply("Your reminder has been set. I will remind you in " + splitMessage[0].slice(0, -1) + "days.");
+        setTimeout(reminder, msDelay);
+        break;
+      }
+    }
   }
 }
 
